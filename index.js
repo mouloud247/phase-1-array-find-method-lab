3e3fff9884bcdd17ef8f42333ef95c88fe329c27
Mouloud Mecheter <mouloudmecheter@gmail.com>
2023-08-06 22:55:42 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },]
+
+function superbowlWin(teamRecords) {
+
+    let bet = function (element) {
+        return (element.result === "W")
+    }
+
+    let b = teamRecords.find(bet);
+    return typeof b === "object" ? b.year : b;
+}
+
+
+console.log(superbowlWin(record));
+
